8092cc4a82f2f2cae0cbe486f976e4daa7949a97
Sumayea Rahman <shorbonam104@gmail.com>
2023-05-21 14:57:15 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import Header from './component/Header/Header';
 
 const App = () => {
     return (
-        <div>
-            <button className="btn btn-active">Button</button>
-            <button className="btn btn-active btn-primary">Button</button>
-            <button className="btn btn-active btn-secondary">Button</button>
-        </div>
+        <>
+            <Header/>
+        </>
     );
 };
 
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center font-bold my-4'>AI Universe Hub</h1>
+        </div>
+    );
+};
+
+export default Header;
